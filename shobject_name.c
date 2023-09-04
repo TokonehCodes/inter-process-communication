9d34d3a70b849930ffa655e97f52d1fae9f59ce1
TokonehCodes <138306980+TokonehCodes@users.noreply.github.com>
2023-09-04 16:17:21 +0100

    Update shobject_name.c

--- shobject_name.c
+++ shobject_name.c
@@ -1,5 +1,4 @@
 /*
- * Replace the following string of 0s with your student number
  * 200300469
  */
 #include <stdlib.h>
@@ -12,12 +11,6 @@ const char* SHOBJ_NAME_FORMAT = "/csc2035.%.9s.%.12s";
 /* 
  * TODO you must implement this function.
  *
- * Hints:
- * - from its specification in shobject_name.h this function may seem 
- *      complicated, in fact almost everything will be covered in C 
- *      programming module materials
- * - there is a library function for getting the current user from the 
- *      environment (use "anon" if the function returns NULL)
  */
 char* shobject_name(char* name_buf, const char* label) {
 	
